dba4947eab8a6244d43906e620089f27dbadd587
Lethaline <LemmensLouis@outlook.fr>
2022-07-19 22:53:40 +0200

    lolemmen

--- main.c
+++ main.c
@@ -6,19 +6,47 @@
 /*   By: lolemmen <lolemmen@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 11:12:10 by lolemmen          #+#    #+#             */
-/*   Updated: 2022/07/09 19:41:45 by lolemmen         ###   ########.fr       */
+/*   Updated: 2022/07/18 14:41:38 by lolemmen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Includes/libft.h"
 #include "stdio.h"
-#include "ctype.h"
+#include "string.h"
+
+size_t ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char *str;
+	int	i;
+
+	str = (char *)s;
+	i = (ft_strlen(str) - 1);
+	while (i >= 0)
+	{
+		if (str[i] == c)
+			return ((char *)&str[i]);
+		i--;
+	}
+	if (str[i] == 0 && c == 0)
+		return ((char *)&str[i]);
+	return (NULL);
+}
+
 
 int	main(void)
 {
-	char *c = "-154.5555.555";
+	char *c = "Bonjour";
 
-	printf("mine : %f\n", ft_atof(c));
-	printf("your : %f\n", atof(c));
+	printf("mine : %s\n", ft_strrchr(c, '\0'));
+	printf("your : %s\n", strrchr(c, '\0'));
 	return (0);
 }
